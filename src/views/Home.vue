039df0696f18236b49466475c5f35dccaa46fcb8
Gautier Lemasson <gautier.lemasson@intech.lu>
2020-03-26 20:14:50 +0100

    Home page : set up back-end calls

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -71,17 +71,18 @@
 
 
 <script>
+  import axios from 'axios';
   export default {
     name: 'Home',
     data() {
       return {
         row1 : {
           label: "",
-          images_url: []
+          images_url: [{id: 0, image_url: ""}]
         },
         row2: {
           label: "",
-          images_url: []
+          images_url: [{id: 0, image_url: ""}]
         },
         row3 : {
           label1 : "",
@@ -89,34 +90,55 @@
         },
         row4: {
           label: "",
-          images_url: []
+          images_url: [{id: 0, image_url: ""}, {id: 1, image_url: ""}, {id: 2, image_url: ""}]
         }
       }
     },
     created() {
-      // MOCK row 1
-      this.row1.label = "Top jeu";
-      this.row1.images_url =
-              [{
-                id: 1,
-                image_url: "http://localhost:1991/images/games/8.jpg"
-              }];
-      // MOCK row 2
-      this.row2.label = "Top joueur";
-      this.row2.images_url =
-              [{
-                id: 1,
-                image_url: "http://localhost:1991/images/players/3.jpg"
-              }];
-      // MOCK row 3
-      this.row3.label1 = "Total parties jouées";
-      this.row3.label2 = 12345;
-      // MOCK row 4
-      this.row4.label = "Derniers jeux joués";
-      this.row4.images_url =
-              [{id: 1,image_url: "http://localhost:1991/images/games/8.jpg"},
-        {id: 2, image_url: "http://localhost:1991/images/games/8.jpg"},
-        {id: 3, image_url: "http://localhost:1991/images/games/8.jpg"}];
+      // Get TOP game
+      axios.get('http://localhost:1991/games/topGame')
+              .then(res => {
+                console.log(res);
+                this.row1.label = "Top jeu";
+                this.row1.images_url =
+                        [{
+                          id: 1,
+                          image_url: `http://localhost:1991/images/games/${res.data.id}.jpg`
+                        }];
+              })
+              .catch(err => console.log(err));
+      // Get TOP player
+      axios.get('http://localhost:1991/players/topPlayer')
+              .then(res => {
+                console.log(res);
+                this.row2.label = "Top joueur";
+                this.row2.images_url =
+                        [{
+                          id: 1,
+                          image_url: `http://localhost:1991/images/players/${res.data.id}.jpg`
+                        }];
+              })
+              .catch(err => console.log(err));
+      // Get total game played
+      axios.get('http://localhost:1991/plays/total')
+              .then(res => {
+                console.log(res);
+                this.row3.label1 = "Total parties jouées";
+                this.row3.label2 = res.data;
+              })
+              .catch(err => console.log(err));
+      // Get last three played games
+      axios.get('http://localhost:1991/games/lastThree')
+              .then(res => {
+                // TODO deal when only one game is returned (i.e. it was played 3 times the last time)
+                console.log(res);
+                this.row4.label = "Derniers jeux joués";
+                this.row4.images_url  =
+                        [{id: 1,image_url: `http://localhost:1991/images/games/${res.data[0].id}.jpg`},
+                          {id: 2, image_url: `http://localhost:1991/images/games/${res.data[1].id}.jpg`},
+                          {id: 3, image_url: `http://localhost:1991/images/games/${res.data[2].id}.jpg`}];
+              })
+              .catch(err => console.log(err));
     }
   }
 </script>
