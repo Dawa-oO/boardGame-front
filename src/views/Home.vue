b205732d38d55a87ccf8e1146a38270f0aa6a68a
Gautier Lemasson <gautier.lemasson@intech.lu>
2020-03-28 17:42:48 +0100

    Switch Home to use components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,69 +2,25 @@
   <div class="home">
     <!-- Page Content -->
     <div class="container">
+      <!-- 1st row -->
       <div class="row">
-        <!-- Team Member 1 -->
-        <div class="col-xl-4 col-md-6 mb-4">
-          <div class="card border-0 shadow">
-            <img :src="row1.images_url[0].image_url" class="card-img-top image" alt="...">
-            <div class="card-body text-center">
-              <h5 class="card-title mb-0">{{row1.label}}</h5>
-            </div>
-          </div>
-        </div>
-        <!-- Team Member 2 -->
-        <div class="col-xl-4 col-md-6 mb-4">
-          <div class="card border-0 shadow">
-            <img :src="row2.images_url[0].image_url" class="card-img-top image" alt="...">
-            <div class="card-body text-center">
-              <h5 class="card-title mb-0">{{row2.label}}</h5>
-            </div>
-          </div>
-        </div>
-        <!-- Team Member 2 -->
-        <div class="col-xl-4 col-md-6 mb-4">
-          <div class="card border-0 shadow to-bottom full-height">
-            <div class="card-body text-center to-inherit">
-              <h1 class="card-title mb-0">{{row3.label2}}</h1>
-            </div>
-            <div class="card-body text-center to-bottom">
-              <h5 class="card-title mb-0">{{row3.label1}}</h5>
-            </div>
-          </div>
-        </div>
+        <!-- Card 1x1 -->
+        <CardImage v-bind:row="row1" />
+        <!-- Card 2x1 -->
+        <CardImage v-bind:row="row2" />
+        <!-- Card 3x1 -->
+        <CardText v-bind:row="row3" />
       </div>
-      <!-- /.row -->
 
+      <!-- 2nd row -->
       <div class="row">
-        <!-- Team Member 1 -->
-        <div class="col-xl-4 col-md-6 mb-4">
-          <div class="card border-0 shadow">
-            <img :src="row4.images_url[0].image_url" class="card-img-top image" alt="...">
-            <div class="card-body text-center">
-              <h5 class="card-title mb-0">{{row4.label}}</h5>
-            </div>
-          </div>
-        </div>
-        <!-- Team Member 2 -->
-        <div class="col-xl-4 col-md-6 mb-4">
-          <div class="card border-0 shadow">
-            <img :src="row4.images_url[1].image_url" class="card-img-top image" alt="...">
-            <div class="card-body text-center">
-              <h5 class="card-title mb-0">{{row4.label}}</h5>
-            </div>
-          </div>
-        </div>
-        <!-- Team Member 3 -->
-        <div class="col-xl-4 col-md-6 mb-4">
-          <div class="card border-0 shadow">
-            <img :src="row4.images_url[2].image_url" class="card-img-top image" alt="...">
-            <div class="card-body text-center">
-              <h5 class="card-title mb-0">{{row4.label}}</h5>
-            </div>
-          </div>
-        </div>
+        <!-- Card 1x2 -->
+        <CardImage v-bind:row="row4" />
+        <!-- Card 2x2 -->
+        <CardImage v-bind:row="row5" />
+        <!-- Card 2x2 -->
+        <CardImage v-bind:row="row6" />
       </div>
-      <!-- /.row -->
     </div>
   </div>
 </template>
@@ -72,17 +28,24 @@
 
 <script>
   import axios from 'axios';
+  import CardImage from "../components/CardImage";
+  import CardText from "../components/CardText";
+
   export default {
     name: 'Home',
+    components: {
+      CardImage,
+      CardText
+    },
     data() {
       return {
         row1 : {
           label: "",
-          images_url: [{id: 0, image_url: ""}]
+          image_url: ""
         },
         row2: {
           label: "",
-          images_url: [{id: 0, image_url: ""}]
+          image_url: ""
         },
         row3 : {
           label1 : "",
@@ -90,7 +53,15 @@
         },
         row4: {
           label: "",
-          images_url: [{id: 0, image_url: ""}, {id: 1, image_url: ""}, {id: 2, image_url: ""}]
+          image_url: ""
+        },
+        row5: {
+          abel: "",
+          image_url: ""
+        },
+        row6: {
+          label: "",
+          image_url: ""
         }
       }
     },
@@ -99,22 +70,14 @@
       axios.get('http://localhost:1991/games/topGame')
               .then(res => {
                 this.row1.label = "Top jeu";
-                this.row1.images_url =
-                        [{
-                          id: 1,
-                          image_url: `http://localhost:1991/images/games/${res.data.id}.jpg`
-                        }];
+                this.row1.image_url = `http://localhost:1991/images/games/${res.data.id}.jpg`;
               })
               .catch(err => console.log(err));
       // Get TOP player
       axios.get('http://localhost:1991/players/topPlayer')
               .then(res => {
                 this.row2.label = "Top joueur";
-                this.row2.images_url =
-                        [{
-                          id: 1,
-                          image_url: `http://localhost:1991/images/players/${res.data.id}.jpg`
-                        }];
+                this.row2.image_url = `http://localhost:1991/images/players/${res.data.id}.jpg`;
               })
               .catch(err => console.log(err));
       // Get total game played
@@ -128,10 +91,13 @@
       axios.get('http://localhost:1991/games/lastThree')
               .then(res => {
                 this.row4.label = "Derniers jeux joués";
-                this.row4.images_url  =
-                        [{id: 1,image_url: `http://localhost:1991/images/games/${res.data[0].id}.jpg`},
-                          {id: 2, image_url: `http://localhost:1991/images/games/${res.data[1].id}.jpg`},
-                          {id: 3, image_url: `http://localhost:1991/images/games/${res.data[2].id}.jpg`}];
+                this.row4.image_url  = `http://localhost:1991/images/games/${res.data[0].id}.jpg`;
+
+                this.row5.label = "Derniers jeux joués";
+                this.row5.image_url  = `http://localhost:1991/images/games/${res.data[1].id}.jpg`;
+
+                this.row6.label = "Derniers jeux joués";
+                this.row6.image_url  = `http://localhost:1991/images/games/${res.data[2].id}.jpg`;
               })
               .catch(err => console.log(err));
     }
@@ -142,25 +108,5 @@
   .home {
     text-align: left;
     margin-top: 5%;
-  }
-  .image {
-    object-fit: contain;
-    width: 300px;
-    height: 300px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 5%;
-  }
-  .to-inherit {
-    height: inherit;
-  }
-  .full-height {
-    height: 100%;
-  }
-  .full-height h1 {
-    margin-top: 40%;
-  }
-  .card {
-    box-shadow: 0 .5rem 1rem rgba(0,0,0,.15)!important;
   }
 </style>
